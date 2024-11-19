a711b2de8892e455c670cded03f71a47fd9ca3db
Ben Purinton <ben.purinton@gmail.com>
2024-11-19 16:45:47 +0000

    update erd and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_003124) do
+ActiveRecord::Schema[7.1].define(version: 2023_08_03_003124) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
